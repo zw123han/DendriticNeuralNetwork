1fab0f7f849c14fd8baa595ec65ed065a50e71a2
Ziwen <ziwen.han@mail.utoronto.ca>
2022-04-13 16:31:56 -0400

    training loop update

--- src/training_loop.py
+++ src/training_loop.py
@@ -1,3 +1,4 @@
+
 import math
 import numpy as np
 import torch
@@ -7,8 +8,9 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import torchvision
 
+
 def load_datasets(args):
-  if(args.dataset == "minist"):
+  if(args["dataset"] == "mnist"):
     mnist_train = datasets.MNIST('data', train=True, download=True,
                           transform = torchvision.transforms.Compose([
                             torchvision.transforms.ToTensor(),
@@ -39,22 +41,29 @@ def load_datasets(args):
 
     dataiter = iter(train_dl)
     train_x, train_y = dataiter.next()
-    train_x = nn.Upsample(size=(32, 32))(train_x)
+    train_x = nn.Upsample(size=(32, 32))(train_x).view(-1, 32*32) # flatten
 
 
 
     dataiter = iter(validation_dl)
     validation_x, validation_y = dataiter.next()
-    validation_x = nn.Upsample(size=(32, 32))(validation_x)
+    validation_x = nn.Upsample(size=(32, 32))(validation_x).view(-1, 32*32)
 
 
     dataiter = iter(test_dl)
     test_x, test_y = dataiter.next()
-    test_x = nn.Upsample(size=(32, 32))(test_x)
+    test_x = nn.Upsample(size=(32, 32))(test_x).view(-1, 32*32)
 
 
     return train_x, validation_x, test_x, train_y, validation_y, test_y
 
+def accuracy(output, labels):
+
+    # From programming assignment 4.
+    predictions = output.max(1)[1].type_as(labels)
+    correct = predictions.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
 
 def train(model, args):
   # model is the model we are going to train.
@@ -65,17 +74,17 @@ def train(model, args):
   valid_losses = []
   valid_accs = []
   # Load optimizer, and loss.
-  optimizer = args['optimizer']()
-  loss = args['loss']()
-  def accuracy(output, labels):
+  #optimizer = args['optimizer']() not using function pointer
+  #loss = args['loss']()
 
-    # From programming assignment 4.
-    predictions = output.max(1)[1].type_as(labels)
-    correct = predictions.eq(labels).double()
-    correct = correct.sum()
-    return correct / len(labels)
 
-  train_x, validation_x, test_x, train_y, validation_y, test_y = load_datasets(args)
+  # train_x, validation_x, test_x, train_y, validation_y, test_y = load_datasets(args)
+
+  if args["optimizer"] == "adam":
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+  if args["criterion"] == "cross entropy":
+    loss = nn.CrossEntropyLoss()
 
   for epoch in range(args["epochs"]):
     model.train()
@@ -100,3 +109,16 @@ def train(model, args):
     print("*****************************Train Loss: {}, Train Accuracy: {}".format(str(loss_train), str(acc_train)))
 
     print("*****************************Validation Loss: {}, Validation Accuracy: {}".format(str(loss_val), str(acc_val)))
+
+args = {
+    "optimizer": "adam",
+    "dataset": "mnist",
+    "criterion": "cross entropy",
+    "epochs": 100
+}
+
+train_x, validation_x, test_x, train_y, validation_y, test_y = load_datasets(args)
+
+model = MLNClassifier(32*32, 10)
+train(model, args)
+
